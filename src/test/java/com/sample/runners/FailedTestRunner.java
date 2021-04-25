b645b4ff64093cb35d25c23b0b1e7ca990fdd06e
MEHMET OLGUN <sweetly35@gmail.com>
2021-04-25 17:14:41 +0300

    added packages

---
+++ src/test/java/com/sample/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.sample.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/failed-html-report"},
+        features = "@target/rerun.txt",
+        glue = "com/sample/step_definitions"
+)
+public class FailedTestRunner {
+}
