1ec78c76b2b9a93a0b8fee758fefd3b39a844c5a
Mehmet OLGUN <sweetly35@gmail.com>
2021-06-29 11:11:10 +0300

    adjusted some comment

--- src/test/java/com/sample/pages/BasePage.java
+++ src/test/java/com/sample/pages/BasePage.java
@@ -86,9 +86,9 @@ public abstract class  BasePage {
     }
 
     /**
-     * This method will navigate user to the specific module in vytrack application.
+     * This method will navigate user to the specific module in the application.
      * For example: if tab is equals to Activities, and module equals to Calls,
-     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
+     * Then method will navigate user to this page: http://qa2.*******.com/call/
      *
      * @param tab
      * @param module
